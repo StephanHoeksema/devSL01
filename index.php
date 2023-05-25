b7b10821e9d20f87c15305054dd25281343d9565
Stephan Paul Hoeksema <s.hoeksema@windesheim.nl>
2023-05-25 18:06:41 +0200

    start college

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 /**
  * Variabelen
  */
+//
+//echo "<HR><H1>VARIABELE</H1><br><hr>";
+//
+///**
+//* integer
+//**/
+//
+//$age = 18;
+//
+//echo "Integer: $age<br><hr>";
+//
+///**
+// * float
+// **/
+//
+//$pi = 3.14;
+//$result = $age / $pi;
+//
+//echo "Float: $age " . "/" . " $pi " . "= $result <br><hr>";
 
-echo "<HR><H1>VARIABELE</H1><br><hr>";
-
-/**
-* integer
-**/
-
-$age = 18;
-
-echo "Integer: $age<br><hr>";
-
-/**
- * float
- **/
-
-$pi = 3.14;
-$result = $age / $pi;
-
-echo "Float: $age " . "/" . " $pi " . "= $result <br><hr>";
-
-require 'views/index.view.php';
+require 'views/portret.view.php';
